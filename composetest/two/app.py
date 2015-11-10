2c01ac6f09eef08afa0df63d542f56575ff3885a
Arialdo Martini <arialdomartini@gmail.com>
2015-11-10 16:27:06 +0100

    Two independent app, sharing a redis storage

--- composetest/two/app.py
+++ composetest/two/app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route("/")
 def hello():
     redis.incr('hits')
-    return "Hello! This page has been visited %s times"  % redis.get('hits')
+    return "Hello! This is the App 2. Pages have been visited %s times"  % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
